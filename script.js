34f69520c0d032378473c447a49773254767eaa8
lawmadesimple101 <lawcore101@gmail.com>
2024-10-30 12:28:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ document.getElementById('quizForm').addEventListener('submit', function(event) {
     // Correct answers for each question
     const correctAnswers = {
         q1: 'B',
-        q2: 'C',
-        q3: 'A',
+        q2: 'B',
+        q3: 'B',
         q4: 'B',
-        q5: 'B'
+        q5: 'A'
     };
 
     // Check each question
